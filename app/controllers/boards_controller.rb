ddaaac56946f686456a1e8155d202cd93cd87a8e
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-10-03 20:02:32 +0900

    boardcreateアクション作成

--- app/controllers/boards_controller.rb
+++ app/controllers/boards_controller.rb
@@ -9,7 +9,18 @@ class BoardsController < ApplicationController
   end
 
   def create
+    @board = Board.new(board_params)
+    if @board.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
+  private
+
+  def board_params
+    params.require(:board).permit(:title, :information, :genre_id, :image).merge(user_id: current_user.id)
+  end
 
 end
