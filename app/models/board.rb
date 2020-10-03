21cc11912df840cdc614e57f2ef43afc0a3e7370
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-10-03 16:26:20 +0900

    ActiveHash導入

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,3 +1,11 @@
 class Board < ApplicationRecord
   belongs_to :user
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :genre
+
+  validates :title, :information, :genre, presence: true
+  with_options numericality: { other_than: 1 } do
+    validates :genre_id
+  end
 end
