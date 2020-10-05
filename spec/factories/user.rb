8c4485753423cc4327813ddc080b0f38dfb436ae
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-10-05 18:31:52 +0900

    rubocop実行

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    name                  {Faker::Name.initials}
+    name                  { Faker::Name.initials}
     email                 {Faker::Internet.free_email}
     password              {Faker::Internet.password}
     password_confirmation {password}
