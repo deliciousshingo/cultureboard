4cd6146338e973aea045fe05a6aa906def6989a2
deliciousshingo <67893129+deliciousshingo@users.noreply.github.com>
2020-10-12 19:12:04 +0900

    コントローラー設定、リファクタリング

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,29 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
 
-def show
-  @boards = current_user.boards
-end
+  def show
+    @boards = @user.boards
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      bypass_sign_in(@user)
+      redirect_to user_path(@user.id)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
